abb97ec5cb49c7231bceddde4836170c1d9724b6
joseibero98 <88434377+joseibero98@users.noreply.github.com>
2023-11-12 09:59:15 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var secciones = document.getElementsByClassName('seccion');
+
+    for (var i = 1; i < secciones.length; i++) {
+        secciones[i].style.display = 'none';
+    }
+
+    document.getElementById('menu').addEventListener('click', function(event) {
+        if (event.target.tagName === 'A') {
+            var targetId = event.target.getAttribute('href').substring(1);
+            mostrarSeccion(targetId);
+        }
+    });
+
+    function mostrarSeccion(id) {
+        for (var i = 0; i < secciones.length; i++) {
+            secciones[i].style.display = 'none';
+        }
+
+        document.getElementById(id).style.display = 'block';
+    }
+});
